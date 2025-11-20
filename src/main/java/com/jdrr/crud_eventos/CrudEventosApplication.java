3cf470154a7419e35043e04b7185130d619f4546
Jhon Deivid Rojas Rodriguez <jhonrojasr253@gmail.com>
2025-11-20 10:16:23 -0500

    Commit initial

---
+++ src/main/java/com/jdrr/crud_eventos/CrudEventosApplication.java
@@ -0,0 +1,13 @@
+package com.jdrr.crud_eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jdrr/crud_eventos/CrudEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdrr.crud_eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
